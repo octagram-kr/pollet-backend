a17bd2f852327a5a7e5823666d0400c1b4a8f7be
Friox <devfriox@gmail.com>
2025-08-28 00:49:58 +0900

    feat: 설문 도메인 엔티티 추가 (#16)

---
+++ src/main/java/com/octagram/pollet/survey/domain/model/SurveyPointHistory.java
@@ -0,0 +1,38 @@
+package com.octagram.pollet.survey.domain.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder(toBuilder = true)
+@Table(name = "survey_point_history")
+public class SurveyPointHistory {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "survey_id", nullable = false)
+	private Survey survey;
+
+	// TODO: 대상 회원 연관관계 추가
+
+	@Column(nullable = false)
+	private Long amount;
+}
---
+++ src/main/java/com/octagram/pollet/survey/domain/model/type/RewardType.java
@@ -0,0 +1,5 @@
+package com.octagram.pollet.survey.domain.model.type;
+
+public enum RewardType {
+	POINT, GIFTICON
+}
