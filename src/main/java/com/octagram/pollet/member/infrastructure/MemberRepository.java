b9e8b71157c3f27c9d89e30435bd9db6ad1bc79c
sseen2 <kse10023@naver.com>
2025-08-28 01:09:59 +0900

    refactor: Member 패키지 이동 (#15)

--- src/main/java/com/octagram/pollet/member/infrastructure/MemberRepository.java
+++ src/main/java/com/octagram/pollet/member/infrastructure/MemberRepository.java
@@ -2,7 +2,7 @@ package com.octagram.pollet.member.infrastructure;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.octagram.pollet.member.domain.Member;
+import com.octagram.pollet.member.domain.model.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
 }
