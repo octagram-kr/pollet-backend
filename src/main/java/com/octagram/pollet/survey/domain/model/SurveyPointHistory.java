b445ecd6f0e73250206d321fbeaf25cb01acfe74
sseen2 <kse10023@naver.com>
2025-08-29 02:44:48 +0900

    refactor: import 구문 Member 패키지 수정 (#15)

--- src/main/java/com/octagram/pollet/survey/domain/model/SurveyPointHistory.java
+++ src/main/java/com/octagram/pollet/survey/domain/model/SurveyPointHistory.java
@@ -1,6 +1,6 @@
 package com.octagram.pollet.survey.domain.model;
 
-import com.octagram.pollet.member.domain.Member;
+import com.octagram.pollet.member.domain.model.Member;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
