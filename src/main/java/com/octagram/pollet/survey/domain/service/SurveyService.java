07c65dfafd615d49e8d1d732c13dace5d092564e
Bogeun Kim <mmmjjjkim@naver.com>
2025-08-29 16:15:48 +0900

    feat: 등록 가능한 모든 태그 조회 기능 구현

--- src/main/java/com/octagram/pollet/survey/domain/service/SurveyService.java
+++ src/main/java/com/octagram/pollet/survey/domain/service/SurveyService.java
@@ -1,10 +1,34 @@
 package com.octagram.pollet.survey.domain.service;
 
+import com.octagram.pollet.survey.domain.model.SurveyTag;
+import com.octagram.pollet.survey.dto.TagResponse;
+import com.octagram.pollet.survey.repository.SurveyTagRepository;
+import com.octagram.pollet.survey.repository.TagRepository;
 import org.springframework.stereotype.Service;
-
 import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class SurveyService {
+    private final TagRepository tagRepository;
+
+    @Transactional(readOnly = true)
+    public List<TagResponse> getAllTags() {
+        return tagRepository.findAll().stream()
+                .map(TagResponse::from)
+                .collect(Collectors.toList());
+    }
+
+
+
+
+
+
+
+
 }
--- src/main/java/com/octagram/pollet/survey/presentation/controller/SurveyController.java
+++ src/main/java/com/octagram/pollet/survey/presentation/controller/SurveyController.java
@@ -1,12 +1,26 @@
 package com.octagram.pollet.survey.presentation.controller;
 
+import com.octagram.pollet.survey.domain.service.SurveyService;
+import com.octagram.pollet.survey.dto.TagResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import lombok.RequiredArgsConstructor;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/survey")
 @RequiredArgsConstructor
 public class SurveyController {
+
+    private final SurveyService surveyService;
+
+    @GetMapping("/tags")
+    public ResponseEntity<List<TagResponse>> getAllTags() {
+        List<TagResponse> tags = surveyService.getAllTags();
+        return ResponseEntity.ok(tags);
+    }
+
+
 }
