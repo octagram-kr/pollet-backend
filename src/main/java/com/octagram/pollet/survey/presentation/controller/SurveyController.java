1648ab19558084f7494885ff4e642dec6ec8b484
Bogeun Kim <mmmjjjkim@naver.com>
2025-08-29 16:24:08 +0900

    feat: SurveyController에 Swagger 어노테이션 추가

--- src/main/java/com/octagram/pollet/survey/presentation/controller/SurveyController.java
+++ src/main/java/com/octagram/pollet/survey/presentation/controller/SurveyController.java
@@ -2,6 +2,7 @@ package com.octagram.pollet.survey.presentation.controller;
 
 import com.octagram.pollet.survey.domain.service.SurveyService;
 import com.octagram.pollet.survey.dto.TagResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ public class SurveyController {
     private final SurveyService surveyService;
 
     @GetMapping("/tags")
+    @Operation(summary = "모든 태그 조회", description = "등록 가능한 모든 태그를 조회합니다.")
     public ResponseEntity<List<TagResponse>> getAllTags() {
         List<TagResponse> tags = surveyService.getAllTags();
         return ResponseEntity.ok(tags);
@@ -24,6 +26,7 @@ public class SurveyController {
 
     // 설문조사에 사용된 태그 중복 제거 후 조회
     @GetMapping("/usedTags")
+    @Operation(summary = "설문조사에서 사용된 태그 조회", description = "중복 제거된 설문조사 태그를 조회합니다.")
     public ResponseEntity<List<TagResponse>> getAllUsedTags() {
         List<TagResponse> tags = surveyService.getAllUsedTags();
         return ResponseEntity.ok(tags);
