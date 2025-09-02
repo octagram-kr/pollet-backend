18c8d85d1d24c6baf10d5a6412e88150720106e2
Bogeun Kim <mmmjjjkim@naver.com>
2025-09-02 10:05:33 +0900

    refactor: api 응답구조 수정

--- src/main/java/com/octagram/pollet/survey/presentation/controller/SurveyController.java
+++ src/main/java/com/octagram/pollet/survey/presentation/controller/SurveyController.java
@@ -1,12 +1,11 @@
 package com.octagram.pollet.survey.presentation.controller;
 
+import com.octagram.pollet.global.dto.ApiResponse;
 import com.octagram.pollet.survey.domain.service.SurveyService;
-import com.octagram.pollet.survey.dto.TagResponse;
+import com.octagram.pollet.survey.domain.status.SurveySuccessCode;
+import com.octagram.pollet.survey.presentation.dto.response.TagResponse;
 import io.swagger.v3.oas.annotations.Operation;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import lombok.RequiredArgsConstructor;
 import java.util.List;
 
@@ -19,17 +18,15 @@ public class SurveyController {
 
     @GetMapping("/tags")
     @Operation(summary = "모든 태그 조회", description = "등록 가능한 모든 태그를 조회합니다.")
-    public ResponseEntity<List<TagResponse>> getAllTags() {
+    public ApiResponse<List<TagResponse>> getAllTags() {
         List<TagResponse> tags = surveyService.getAllTags();
-        return ResponseEntity.ok(tags);
+        return ApiResponse.success(SurveySuccessCode.READ_TAGS_SUCCESS, tags);
     }
 
     @GetMapping("/usedTags")
-    @Operation(summary = "설문조사에서 사용된 태그 조회", description = "중복 제거된 설문조사 태그를 조회합니다.")
-    public ResponseEntity<List<TagResponse>> getAllUsedTags() {
+    @Operation(summary = "설문조사에 등록된 태그 조회", description = "중복 제거된 설문조사 태그를 조회합니다.")
+    public ApiResponse<List<TagResponse>> getAllUsedTags() {
         List<TagResponse> tags = surveyService.getAllUsedTags();
-        return ResponseEntity.ok(tags);
+        return ApiResponse.success(SurveySuccessCode.READ_SURVEY_TAGS_SUCCESS, tags);
     }
-
-
 }
