382408bbb0f60c0e2b2261102ec42efcea69a879
Bogeun Kim <mmmjjjkim@naver.com>
2025-09-02 13:37:06 +0900

    refactor: Repository 어노테이션 제거

--- src/main/java/com/octagram/pollet/survey/repository/SurveyTagRepository.java
+++ src/main/java/com/octagram/pollet/survey/repository/SurveyTagRepository.java
@@ -2,8 +2,6 @@ package com.octagram.pollet.survey.repository;
 
 import com.octagram.pollet.survey.domain.model.SurveyTag;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface SurveyTagRepository extends JpaRepository<SurveyTag, Long>, SurveyTagCustomRepository {
 }
--- src/main/java/com/octagram/pollet/survey/repository/SurveyTagRepositoryImpl.java
+++ src/main/java/com/octagram/pollet/survey/repository/SurveyTagRepositoryImpl.java
@@ -5,10 +5,8 @@ import com.octagram.pollet.survey.domain.model.QTag;
 import com.octagram.pollet.survey.domain.model.Tag;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Repository;
 import java.util.List;
 
-@Repository
 @RequiredArgsConstructor
 public class SurveyTagRepositoryImpl implements SurveyTagCustomRepository {
 
