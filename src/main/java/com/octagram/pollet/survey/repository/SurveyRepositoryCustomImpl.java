d7f617f9fe9b2277b5c48b1049bcfff9f484c7a3
Bogeun Kim <mmmjjjkim@naver.com>
2025-09-04 14:49:49 +0900

    refactor: 날짜 필드 수정 (#22)

--- src/main/java/com/octagram/pollet/survey/repository/SurveyRepositoryCustomImpl.java
+++ src/main/java/com/octagram/pollet/survey/repository/SurveyRepositoryCustomImpl.java
@@ -4,6 +4,7 @@ import com.octagram.pollet.survey.domain.model.QSurvey;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @RequiredArgsConstructor
 public class SurveyRepositoryCustomImpl implements SurveyRepositoryCustom {
@@ -14,12 +15,15 @@ public class SurveyRepositoryCustomImpl implements SurveyRepositoryCustom {
     public long countActive(LocalDate today) {
         QSurvey survey = QSurvey.survey;
 
+        LocalDateTime startOfDay = today.atStartOfDay(); // LocalDate -> LocalDateTime 시작 시간
+        LocalDateTime endOfDay = today.atTime(23, 59, 59); // LocalDate -> LocalDateTime 종료 시간
+
         return queryFactory
                 .select(survey.count())
                 .from(survey)
                 .where(
-                        survey.startDate.loe(today),
-                        survey.endDate.goe(today)
+                        survey.startDateTime.loe(endOfDay), // 현재 '끝 시간' 포함 조건 확인
+                        survey.endDateTime.goe(startOfDay)  // 현재 '시작 시간' 포함 조건 확인
                 )
                 .fetchOne();
     }
