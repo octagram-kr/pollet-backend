72f1845873ad54c8760665c64bbc1a6ef4f1994c
Friox <devfriox@gmail.com>
2025-09-04 14:39:23 +0900

    feat: Sample 도메인 삭제 (#30)

--- src/main/java/com/octagram/pollet/sample/domain/repository/SampleRepositoryCustom.java
+++
@@ -1,9 +0,0 @@
-package com.octagram.pollet.sample.domain.repository;
-
-import java.util.Optional;
-
-import com.octagram.pollet.sample.domain.model.Sample;
-
-public interface SampleRepositoryCustom {
-	Optional<Sample> findByIdCustom(Long id);
-}
