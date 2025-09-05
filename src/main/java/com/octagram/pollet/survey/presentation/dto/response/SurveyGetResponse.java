7f5cb64094ee7f0f41a00364a43ff41cde9de6ff
Friox <devfriox@gmail.com>
2025-09-05 13:58:45 +0900

    refactor: 응답 DTO 네이밍 수정, 정적 메서드 삭제 (#33)

---
+++ src/main/java/com/octagram/pollet/survey/presentation/dto/response/SurveyGetResponse.java
@@ -0,0 +1,9 @@
+package com.octagram.pollet.survey.presentation.dto.response;
+
+public record SurveyGetResponse(
+        Long id,
+        String title,
+        Long rewardPoint,
+        Long estimatedTime
+) {
+}
---
+++ src/main/java/com/octagram/pollet/survey/presentation/dto/response/TagGetResponse.java
@@ -0,0 +1,7 @@
+package com.octagram.pollet.survey.presentation.dto.response;
+
+public record TagGetResponse(
+        Long id,
+        String name
+) {
+}
