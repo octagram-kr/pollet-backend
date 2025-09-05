000e87fbccc1a8dc0f274bec899de06ebe3f8299
sseen2 <kse10023@naver.com>
2025-09-05 11:05:42 +0900

    feat: 시큐리티 설정 추가 (#23)

--- src/main/java/com/octagram/pollet/global/config/SecurityConfig.java
+++ src/main/java/com/octagram/pollet/global/config/SecurityConfig.java
@@ -1,13 +1,22 @@
 package com.octagram.pollet.global.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.octagram.pollet.auth.application.CustomUserOAuth2Service;
+import com.octagram.pollet.auth.handler.OAuth2LoginFailureHandler;
+import com.octagram.pollet.auth.handler.OAuth2LoginSuccessHandler;
+import com.octagram.pollet.global.jwt.filter.JwtAuthFilter;
 
 import lombok.RequiredArgsConstructor;
 
@@ -16,18 +25,50 @@ import lombok.RequiredArgsConstructor;
 @EnableWebSecurity
 public class SecurityConfig {
 
+	private static final List<String> ALLOWED_ORIGINS = List.of(
+		"http://localhost:8080"
+		// 프론트엔드 로컬 주소
+		// 프론트엔드 배포 주소
+	);
+	private static final List<String> ALLOWED_METHOD = List.of("GET", "POST", "PUT", "DELETE", "PATCH");
+
+	private final JwtAuthFilter jwtAuthFilter;
 	private final CustomUserOAuth2Service customUserOAuth2Service;
+	private final OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
+	private final OAuth2LoginFailureHandler oAuth2LoginFailureHandler;
 
 	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-		return httpSecurity
+	public SecurityFilterChain securityFilterChain(HttpSecurity http, CorsConfigurationSource corsConfigurationSource)
+		throws Exception {
+		http
+			.cors(cors -> cors.configurationSource(corsConfigurationSource))
 			.csrf(AbstractHttpConfigurer::disable)
 			.authorizeHttpRequests(auth -> {
-				auth.anyRequest().permitAll();
+				auth.anyRequest().permitAll()
+					.requestMatchers("/admin/**").hasRole("ADMIN");
 			})
+			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+			.addFilter(jwtAuthFilter)
 			.oauth2Login(oauth2 -> oauth2
 				.userInfoEndpoint(userInfoEndpointConfig
-					-> userInfoEndpointConfig.userService(customUserOAuth2Service)))
-			.build();
+					-> userInfoEndpointConfig.userService(customUserOAuth2Service))
+				.successHandler(oAuth2LoginSuccessHandler)
+				.failureHandler(oAuth2LoginFailureHandler));
+
+		return http.build();
+	}
+
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration configuration = new CorsConfiguration();
+
+		configuration.setAllowCredentials(true);
+		configuration.setAllowedOrigins(ALLOWED_ORIGINS);
+		configuration.setAllowedMethods(ALLOWED_METHOD);
+		configuration.setAllowedHeaders(List.of("*"));
+
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
 	}
 }
