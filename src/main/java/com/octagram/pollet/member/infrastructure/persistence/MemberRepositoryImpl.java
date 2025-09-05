cf9d72302c746c420b0dfbc0cf2b2069b064c910
sseen2 <kse10023@naver.com>
2025-09-05 16:27:37 +0900

    refactor: 이름 수정 및 어노테이션 추가 (#32)

--- src/main/java/com/octagram/pollet/member/infrastructure/persistence/MemberRepositoryImpl.java
+++ src/main/java/com/octagram/pollet/member/infrastructure/persistence/MemberRepositoryImpl.java
@@ -1,5 +1,7 @@
 package com.octagram.pollet.member.infrastructure.persistence;
 
+import org.springframework.stereotype.Repository;
+
 import com.octagram.pollet.member.domain.model.Member;
 import com.octagram.pollet.member.domain.model.QMember;
 import com.octagram.pollet.member.domain.repository.MemberRepositoryCustom;
@@ -7,8 +9,9 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import lombok.RequiredArgsConstructor;
 
+@Repository
 @RequiredArgsConstructor
-public class MemberRepositoryCustomImpl implements MemberRepositoryCustom {
+public class MemberRepositoryImpl implements MemberRepositoryCustom {
 
 	private final JPAQueryFactory queryFactory;
 
