1eada730aa206829d862dc45b637df638758854e
Friox <devfriox@gmail.com>
2025-09-05 13:54:28 +0900

    feat: 설문조사 조회 시 join을 위한 QueryDSL 메서드 구현 (#33)

--- src/main/java/com/octagram/pollet/survey/infrastructure/SurveyRepositoryImpl.java
+++ src/main/java/com/octagram/pollet/survey/infrastructure/SurveyRepositoryImpl.java
@@ -1,11 +1,16 @@
 package com.octagram.pollet.survey.infrastructure;
 
+import static com.octagram.pollet.gifticon.domain.model.QGifticonProduct.*;
+import static com.octagram.pollet.survey.domain.model.QSurvey.*;
+
 import com.octagram.pollet.survey.domain.model.QSurvey;
+import com.octagram.pollet.survey.domain.model.Survey;
 import com.octagram.pollet.survey.domain.repository.SurveyRepositoryCustom;
 import org.springframework.stereotype.Repository;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
@@ -14,7 +19,7 @@ public class SurveyRepositoryImpl implements SurveyRepositoryCustom {
     private final JPAQueryFactory queryFactory;
 
     @Override
-    public long countActive(LocalDateTime now) {
+    public long getActiveCount(LocalDateTime now) {
         QSurvey survey = QSurvey.survey;
 
         return queryFactory
@@ -25,5 +30,16 @@ public class SurveyRepositoryImpl implements SurveyRepositoryCustom {
                         survey.endDateTime.goe(now)
                 )
                 .fetchOne();
+    }
+
+    @Override
+    public Optional<Survey> findByIdQueryDsl(Long id) {
+        return Optional.ofNullable(
+            queryFactory
+                .selectFrom(survey)
+                .join(survey.gifticonProduct, gifticonProduct)
+                .fetchJoin()
+                .fetchOne()
+        );
     }
 }
