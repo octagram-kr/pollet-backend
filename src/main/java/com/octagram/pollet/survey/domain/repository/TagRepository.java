370168d1fc865c5b6911e5ba24bef25c269587f5
Friox <devfriox@gmail.com>
2025-09-05 13:55:36 +0900

    refactor: 불필요한 어노테이션 삭제 (#33)

--- src/main/java/com/octagram/pollet/survey/domain/repository/TagRepository.java
+++ src/main/java/com/octagram/pollet/survey/domain/repository/TagRepository.java
@@ -2,8 +2,6 @@ package com.octagram.pollet.survey.domain.repository;
 
 import com.octagram.pollet.survey.domain.model.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface TagRepository extends JpaRepository<Tag, Long> {
 }
