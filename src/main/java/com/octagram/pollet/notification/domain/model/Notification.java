26a1c95e86047ef25fe07c46149645146706dc6c
Bogeun Kim <mmmjjjkim@naver.com>
2025-09-08 14:26:44 +0900

    feat: 알림 엔티티 추가 및 연관관계 설정 (#52)

---
+++ src/main/java/com/octagram/pollet/notification/domain/model/Notification.java
@@ -0,0 +1,41 @@
+package com.octagram.pollet.notification.domain.model;
+
+import com.octagram.pollet.global.domain.model.BaseEntity;
+import com.octagram.pollet.member.domain.model.Member;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Table(name = "notification")
+public class Notification extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id", nullable = false)
+	private Member receiver;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id", nullable = true)
+	private Member sender;
+
+	@Column(nullable = false)
+	private String title;
+
+	@Column(nullable = false)
+	private String message;
+
+	@Column(nullable = false)
+	@Enumerated(EnumType.STRING)
+	private NotificationType notificationType;
+
+	@Builder.Default
+	@Column(nullable = false)
+	private boolean isRead = false;
+}
