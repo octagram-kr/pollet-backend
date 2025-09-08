3fb8f034c95af78b6a59341f3b08e8c0a4515262
Bogeun Kim <mmmjjjkim@naver.com>
2025-09-08 14:59:53 +0900

    refactor: 확정된 카테고리로 수정 (#52)

--- src/main/java/com/octagram/pollet/notification/domain/model/NotificationType.java
+++ src/main/java/com/octagram/pollet/notification/domain/model/NotificationType.java
@@ -1,9 +1,5 @@
 package com.octagram.pollet.notification.domain.model;
 
 public enum NotificationType {
-	// 일반, 기프티콘, 인터뷰, 기타
-	GENERAL,
-	GIFTICON,
-	INTERVIEW,
-	ETC
+	GENERAL, GIFTICON, INTERVIEW
 }
