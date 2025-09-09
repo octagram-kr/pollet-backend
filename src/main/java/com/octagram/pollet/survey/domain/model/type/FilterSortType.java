26b21ba07f6f549d7dcdcf1b4a13de47dfe95b73
Friox <devfriox@gmail.com>
2025-09-09 22:14:28 +0900

    refactor: enum 타입 단어 수정 (submission) (#70)

--- src/main/java/com/octagram/pollet/survey/domain/model/type/FilterSortType.java
+++ src/main/java/com/octagram/pollet/survey/domain/model/type/FilterSortType.java
@@ -1,5 +1,5 @@
 package com.octagram.pollet.survey.domain.model.type;
 
 public enum FilterSortType {
-	HIGHEST_POINTS, SHORTEST_TIME, LONGEST_TIME, FEWEST_RESPONSES
+	HIGHEST_POINTS, SHORTEST_TIME, LONGEST_TIME, FEWEST_SUBMISSIONS
 }
