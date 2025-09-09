cdea4139ff03a935ea374e4a17a035233157898d
Friox <devfriox@gmail.com>
2025-09-10 01:46:47 +0900

    refactor: 설문조사 도메인 불필요 메서드 삭제, 반환 타입 수정 (#70)

--- src/main/java/com/octagram/pollet/survey/application/SurveyService.java
+++ src/main/java/com/octagram/pollet/survey/application/SurveyService.java
@@ -9,16 +9,15 @@ import com.octagram.pollet.survey.domain.model.Survey;
 import com.octagram.pollet.survey.domain.repository.QuestionRepository;
 import com.octagram.pollet.survey.domain.status.SurveyErrorCode;
 import com.octagram.pollet.survey.presentation.dto.request.SurveyFilterRequest;
-import com.octagram.pollet.survey.presentation.dto.response.SurveyFilterResponse;
-import com.octagram.pollet.survey.presentation.dto.response.SurveyGetDetailResponse;
-import com.octagram.pollet.survey.presentation.dto.response.SurveyGetResponse;
-import com.octagram.pollet.survey.presentation.dto.response.TagGetResponse;
 import com.octagram.pollet.survey.presentation.dto.response.*;
 import com.octagram.pollet.survey.domain.repository.SurveyRepository;
 import com.octagram.pollet.survey.domain.repository.SurveyTagRepository;
 import com.octagram.pollet.survey.domain.repository.TagRepository;
+import com.octagram.pollet.survey.presentation.dto.response.standard.QuestionResponse;
+import com.octagram.pollet.survey.presentation.dto.response.standard.SurveyResponse;
+import com.octagram.pollet.survey.presentation.dto.response.standard.TagResponse;
+
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import lombok.RequiredArgsConstructor;
@@ -39,30 +38,30 @@ public class SurveyService {
 	private final QuestionMapper questionMapper;
 
 	@Transactional(readOnly = true)
-	public List<TagGetResponse> getAllTags() {
+	public List<TagResponse> getAllTags() {
 		return tagRepository.findAll().stream()
-			.map(tagMapper::toGetResponse)
+			.map(tagMapper::toResponse)
 			.toList();
 	}
 
 	@Transactional(readOnly = true)
-	public List<TagGetResponse> getAllUsedTags() {
+	public List<TagResponse> getAllUsedTags() {
 		return surveyTagRepository.findAllUsedTags().stream()
-			.map(tagMapper::toGetResponse)
+			.map(tagMapper::toResponse)
 			.toList();
 	}
 
 	@Transactional(readOnly = true)
-	public SurveyGetDetailResponse getSurveyById(Long surveyId) {
+	public SurveyResponse getSurveyById(Long surveyId) {
 		Survey survey = surveyRepository.findByIdQueryDsl(surveyId)
 			.orElseThrow(() -> new BusinessException(SurveyErrorCode.SURVEY_NOT_FOUND));
-		return surveyMapper.toGetDetailResponse(survey);
+		return surveyMapper.toSurveyResponse(survey);
 	}
 
 	@Transactional(readOnly = true)
-	public List<TagGetResponse> getTagsBySurveyId(Long surveyId) {
+	public List<TagResponse> getTagsBySurveyId(Long surveyId) {
 		return surveyTagRepository.findBySurveyId(surveyId).stream()
-			.map(surveyTag -> tagMapper.toGetResponse(surveyTag.getTag()))
+			.map(surveyTag -> tagMapper.toResponse(surveyTag.getTag()))
 			.toList();
 	}
 
@@ -70,12 +69,6 @@ public class SurveyService {
 	public long getActiveCount() {
 		LocalDateTime now = LocalDateTime.now();
 		return surveyRepository.getActiveCount(now);
-	}
-
-	@Transactional(readOnly = true)
-	public Page<SurveyGetResponse> searchSurveys(String keyword, Pageable pageable) {
-		return surveyRepository.findByTitleContainingIgnoreCase(keyword, pageable)
-			.map(surveyMapper::toGetResponse);
 	}
 
 	@Transactional(readOnly = true)
@@ -107,7 +100,7 @@ public class SurveyService {
 	}
 
 	@Transactional(readOnly = true)
-	public List<SurveyFilterResponse> filterSurveys(SurveyFilterRequest filter, Pageable pageable) {
+	public List<SurveyResponse> filterSurveys(SurveyFilterRequest filter, Pageable pageable) {
 		List<String> allTags = new ArrayList<>();
 		if (filter.genders() != null) allTags.addAll(filter.genders());
 		if (filter.ages() != null) allTags.addAll(filter.ages());
@@ -115,7 +108,7 @@ public class SurveyService {
 		if (filter.tags() != null) allTags.addAll(filter.tags());
 		List<Survey> surveysResult = surveyRepository.findByFilter(allTags, filter, pageable);
 		return surveysResult.stream()
-			.map(surveyMapper::toFilterResponse)
+			.map(surveyMapper::toSurveyResponse)
 			.toList();
 	}
 
