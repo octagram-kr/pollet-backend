9932597e5e603bb2bb19db25ae04609c8597cf65
Friox <devfriox@gmail.com>
2025-09-09 13:14:25 +0900

    feat: 태그 매퍼 메서드 추가 (#47)

--- src/main/java/com/octagram/pollet/survey/application/mapper/TagMapper.java
+++ src/main/java/com/octagram/pollet/survey/application/mapper/TagMapper.java
@@ -1,12 +1,22 @@
 package com.octagram.pollet.survey.application.mapper;
 
-import org.mapstruct.Mapper;
+import java.util.List;
 
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import com.octagram.pollet.survey.domain.model.SurveyTag;
 import com.octagram.pollet.survey.domain.model.Tag;
 import com.octagram.pollet.survey.presentation.dto.response.TagGetResponse;
+import com.octagram.pollet.survey.presentation.dto.response.TagInSurveyFilterResponse;
 
 @Mapper
 public interface TagMapper {
 
 	TagGetResponse toGetResponse(Tag tag);
+
+	@Mapping(source = "tag.name", target = "name")
+	TagInSurveyFilterResponse toSurveyFilterResponse(SurveyTag surveyTag);
+
+	List<TagInSurveyFilterResponse> toSurveyFilterResponses(List<SurveyTag> surveyTags);
 }
