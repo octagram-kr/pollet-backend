f59ce7ad4c291a9842ead9fe94d82067c174b04a
Friox <devfriox@gmail.com>
2025-09-10 01:40:32 +0900

    feat: 기본 DTO 생성 및 이동 (#70)

--- src/main/java/com/octagram/pollet/survey/presentation/dto/response/standard/QuestionOptionResponse.java
+++ src/main/java/com/octagram/pollet/survey/presentation/dto/response/standard/QuestionOptionResponse.java
@@ -1,6 +1,9 @@
-package com.octagram.pollet.survey.presentation.dto.response;
+package com.octagram.pollet.survey.presentation.dto.response.standard;
+
+import java.time.LocalDateTime;
 
 public record QuestionOptionResponse(
+	Long id,
 	Long order,
 	String content,
 	String imageUrl,
