aa160e954b1a7d0c396a9704f73d49071520e840
sseen2 <kse10023@naver.com>
2025-09-10 21:41:38 +0900

    refactor: 불필요한 로직 삭제 (#57)

--- src/main/java/com/octagram/pollet/member/domain/repository/MemberRepositoryCustom.java
+++ src/main/java/com/octagram/pollet/member/domain/repository/MemberRepositoryCustom.java
@@ -1,8 +1,4 @@
 package com.octagram.pollet.member.domain.repository;
 
-import com.octagram.pollet.member.domain.model.Member;
-
 public interface MemberRepositoryCustom {
-
-	Member findByMemberNickname(String nickname);
 }
--- src/main/java/com/octagram/pollet/member/infrastructure/persistence/MemberRepositoryImpl.java
+++ src/main/java/com/octagram/pollet/member/infrastructure/persistence/MemberRepositoryImpl.java
@@ -2,8 +2,6 @@ package com.octagram.pollet.member.infrastructure.persistence;
 
 import org.springframework.stereotype.Repository;
 
-import com.octagram.pollet.member.domain.model.Member;
-import com.octagram.pollet.member.domain.model.QMember;
 import com.octagram.pollet.member.domain.repository.MemberRepositoryCustom;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
@@ -14,13 +12,4 @@ import lombok.RequiredArgsConstructor;
 public class MemberRepositoryImpl implements MemberRepositoryCustom {
 
 	private final JPAQueryFactory queryFactory;
-
-	@Override
-	public Member findByMemberNickname(String nickname) {
-		QMember member = QMember.member;
-		return queryFactory
-			.selectFrom(member)
-			.where(member.nickname.eq(nickname))
-			.fetchOne();
-	}
 }
