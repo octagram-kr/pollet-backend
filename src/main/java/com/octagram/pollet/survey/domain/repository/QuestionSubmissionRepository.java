f24877f5e2d507c832903aecd7fc2d8b7b11e5c7
Bogeun Kim <mmmjjjkim@naver.com>
2025-09-11 09:58:51 +0900

    refactor: QueryDSL을 적용하여 타입 안정성 보장 (#50)

--- src/main/java/com/octagram/pollet/survey/domain/repository/QuestionSubmissionRepository.java
+++ src/main/java/com/octagram/pollet/survey/domain/repository/QuestionSubmissionRepository.java
@@ -3,13 +3,8 @@ package com.octagram.pollet.survey.domain.repository;
 import com.octagram.pollet.survey.domain.model.Question;
 import com.octagram.pollet.survey.domain.model.QuestionSubmission;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import java.util.List;
 
-public interface QuestionSubmissionRepository extends JpaRepository<QuestionSubmission, Long> {
+public interface QuestionSubmissionRepository extends JpaRepository<QuestionSubmission, Long>, QuestionSubmissionRepositoryCustom {
+
 	int countByQuestion(Question question);
-
-	@Query("select qs.answer from QuestionSubmission qs where qs.question = :question and qs.answer is not null")
-	List<String> findAnswersByQuestion(@Param("question") Question question);
 }
