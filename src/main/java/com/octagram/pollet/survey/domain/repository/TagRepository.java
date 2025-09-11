8ecc4834d600c84a1ff5863863fcf6a659f73dd9
Friox <devfriox@gmail.com>
2025-09-11 13:26:40 +0900

    feat: 태그 레포지토리 메서드 추가 (#48)

--- src/main/java/com/octagram/pollet/survey/domain/repository/TagRepository.java
+++ src/main/java/com/octagram/pollet/survey/domain/repository/TagRepository.java
@@ -1,8 +1,11 @@
 package com.octagram.pollet.survey.domain.repository;
 
+import java.util.Optional;
+
 import com.octagram.pollet.survey.domain.model.Tag;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TagRepository extends JpaRepository<Tag, Long> {
+	Optional<Tag> findByName(String name);
 }
