857382fa96ea4fe6e71c7a3f7e9d796ce4f4152d
Bogeun Kim <mmmjjjkim@naver.com>
2025-09-11 10:00:52 +0900

    feat: Slice를 활용한 무한스크롤 기능 적용 (#50)

--- src/main/java/com/octagram/pollet/survey/domain/repository/QuestionRepository.java
+++ src/main/java/com/octagram/pollet/survey/domain/repository/QuestionRepository.java
@@ -1,7 +1,11 @@
 package com.octagram.pollet.survey.domain.repository;
 
-import com.octagram.pollet.survey.domain.model.QuestionOption;
+import com.octagram.pollet.survey.domain.model.Question;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface QuestionRepository extends JpaRepository<QuestionOption, Long>, QuestionRepositoryCustom {
+public interface QuestionRepository extends JpaRepository<Question, Long>, QuestionRepositoryCustom {
+
+	Slice<Question> findBySurveyId(Long surveyId, Pageable pageable);
 }
