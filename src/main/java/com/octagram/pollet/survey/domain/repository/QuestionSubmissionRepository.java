02d1dd59ad0114188fea438d79c602a19ccf6e9f
Bogeun Kim <mmmjjjkim@naver.com>
2025-09-11 09:26:18 +0900

    feat: 특정 질문에 대해 제출된 답변 조회 쿼리 메서드 추가 (#50)

--- src/main/java/com/octagram/pollet/survey/domain/repository/QuestionSubmissionRepository.java
+++ src/main/java/com/octagram/pollet/survey/domain/repository/QuestionSubmissionRepository.java
@@ -3,11 +3,13 @@ package com.octagram.pollet.survey.domain.repository;
 import com.octagram.pollet.survey.domain.model.Question;
 import com.octagram.pollet.survey.domain.model.QuestionSubmission;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface QuestionSubmissionRepository extends JpaRepository<QuestionSubmission, Long> {
 	int countByQuestion(Question question);
 
-	List<String> findAnswersByQuestion(Question question);
+	@Query("select qs.answer from QuestionSubmission qs where qs.question = :question and qs.answer is not null")
+	List<String> findAnswersByQuestion(@Param("question") Question question);
 }
