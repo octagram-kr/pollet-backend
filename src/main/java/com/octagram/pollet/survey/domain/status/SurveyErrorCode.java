22fe17d5b027ed0760a144413749ac7daadda520
Bogeun Kim <mmmjjjkim@naver.com>
2025-09-12 10:25:23 +0900

    feat: 설문조사 상태코드 추가 (#50)

--- src/main/java/com/octagram/pollet/survey/domain/status/SurveyErrorCode.java
+++ src/main/java/com/octagram/pollet/survey/domain/status/SurveyErrorCode.java
@@ -19,7 +19,8 @@ public enum SurveyErrorCode implements BaseCode {
 	INVALID_ACCESS(HttpStatus.FORBIDDEN, "SE_006", "잘못된 접근입니다."),
 	QUESTION_NOT_FOUND(HttpStatus.NOT_FOUND, "SE_007", "설문조사의 대표 질문이 존재하지 않습니다."),
 	SUBMISSION_NOT_FOUND(HttpStatus.NOT_FOUND, "SE_008", "요청한 제출 내역이 존재하지 않습니다."),
-	INVALID_SUBMISSION(HttpStatus.BAD_REQUEST, "SE_009", "제출 내역이 해당 설문과 일치하지 않습니다.");
+	INVALID_SUBMISSION(HttpStatus.BAD_REQUEST, "SE_009", "제출 내역이 해당 설문과 일치하지 않습니다."),
+	UNAUTHORIZED_ACCESS(HttpStatus.FORBIDDEN, "SE_010", "설문 생성자가 아닙니다.");
 
 	private final HttpStatus status;
 	private final String code;
