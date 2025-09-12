4c56cba31f1d7b64b57a0f3a1b95823577c6f2af
Bogeun Kim <mmmjjjkim@naver.com>
2025-09-12 10:38:30 +0900

    feat: feat: 설문조사 및 제출 데이터 검증 로직 추가 (#50)

--- src/main/java/com/octagram/pollet/survey/domain/repository/SurveySubmissionRepository.java
+++ src/main/java/com/octagram/pollet/survey/domain/repository/SurveySubmissionRepository.java
@@ -3,7 +3,11 @@ package com.octagram.pollet.survey.domain.repository;
 import com.octagram.pollet.survey.domain.model.Survey;
 import com.octagram.pollet.survey.domain.model.SurveySubmission;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface SurveySubmissionRepository extends JpaRepository<SurveySubmission, Long> {
+
 	int countBySurvey(Survey survey);
+
+	Optional<SurveySubmission> findBySurveyIdAndId(Long surveyId, Long submissionId);
 }
