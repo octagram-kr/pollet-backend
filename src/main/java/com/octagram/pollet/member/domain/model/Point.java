29f82bb0acbb3003553b75fe72079085585b6389
sseen2 <kse10023@naver.com>
2025-09-12 22:22:59 +0900

    feat: 포인트, 포인트내역 Entity 추가 (#100)

---
+++ src/main/java/com/octagram/pollet/member/domain/model/Point.java
@@ -0,0 +1,36 @@
+package com.octagram.pollet.member.domain.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder(toBuilder = true)
+@Table(name = "point")
+public class Point {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id", nullable = false)
+	private Member member;
+
+	@Column(nullable = false)
+	private long balance;
+}
---
+++ src/main/java/com/octagram/pollet/member/domain/model/PointHistory.java
@@ -0,0 +1,60 @@
+package com.octagram.pollet.member.domain.model;
+
+import com.octagram.pollet.gifticon.domain.model.GifticonCode;
+import com.octagram.pollet.survey.domain.model.Survey;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder(toBuilder = true)
+@Table(name = "point_history")
+public class PointHistory {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id")
+	private Member member;
+
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "survey_id", nullable = true)
+	private Survey survey;
+
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "gifticon_code_id", nullable = true)
+	private GifticonCode gifticonCode;
+
+	@Column(nullable = true)
+	private Long transactionNumber;
+
+	@Column(nullable = true)
+	private String transactionType;
+
+	@Column(nullable = true)
+	private String transactionDescription;
+
+	@Column(nullable = false)
+	private Long amount;
+
+	@Column(nullable = false)
+	private Long afterAmount;
+}
