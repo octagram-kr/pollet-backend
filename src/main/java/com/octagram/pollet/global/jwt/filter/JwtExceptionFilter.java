26e38e9cc708da7715ef6b0492a10b6a7d6e6a68
Friox <devfriox@gmail.com>
2025-09-13 17:36:17 +0900

    feat: JwtAuthFilter 에서 발생하는 예외를 처리하기 위한 Filter 구현 (#98)

---
+++ src/main/java/com/octagram/pollet/global/jwt/filter/JwtExceptionFilter.java
@@ -0,0 +1,37 @@
+package com.octagram.pollet.global.jwt.filter;
+
+import java.io.IOException;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.octagram.pollet.global.dto.ApiResponse;
+import com.octagram.pollet.global.exception.BusinessException;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class JwtExceptionFilter extends OncePerRequestFilter {
+
+	@Override
+	protected void doFilterInternal(
+		HttpServletRequest request,
+		HttpServletResponse response,
+		FilterChain filterChain
+	) throws ServletException, IOException {
+		try {
+			filterChain.doFilter(request, response);
+		} catch (BusinessException e) {
+			response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+			response.setStatus(e.getErrorCode().getStatus().value());
+			ApiResponse<?> apiResponse = ApiResponse.fail(e.getErrorCode(), e.getCause());
+			final ObjectMapper mapper = new ObjectMapper();
+			mapper.writeValue(response.getOutputStream(), apiResponse);
+		}
+	}
+}
